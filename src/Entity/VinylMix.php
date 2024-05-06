c52156856741e1d91e4e94d243e80d17b78b6a0f
salimabdennebi <148327501+salimabdennebi@users.noreply.github.com>
2024-05-06 11:40:32 +0100

    Generating the VinylMix Entity with make:entity

--- src/Entity/VinylMix.php
+++ src/Entity/VinylMix.php
@@ -14,19 +14,19 @@ class VinylMix
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: false)]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $description = null;
 
-    #[ORM\Column]
+    #[ORM\Column(type: 'text', nullable: false)]
     private ?int $trackCount = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: false)]
     private ?string $genre = null;
 
-    #[ORM\Column]
+    #[ORM\Column(type: 'datetime_immutable', nullable: false)]
     private ?\DateTimeImmutable $createdAt = null;
 
     public function getId(): ?int
